dd3c788d4106e62565ca15f12542b19ad93fa8b8
tormozz48 <tormozz48@gmail.com>
2015-04-23 16:37:25 +0300

    Implement scroll to functionality. Add popover for thumbnails

--- js/app.js
+++ js/app.js
@@ -1,4 +1,9 @@
+$(function () {
+    $('[data-toggle="popover"]').popover()
+});
+
 (function() {
+
     var DeliveryController = function ($log) {
         $log.debug('Initialize delivery controller');
 
@@ -15,11 +20,25 @@
         this.body = 'Наличный и безналичный расчет с НДС';
     };
 
+    var MenuController = function () {
+        this.items = [
+            { target: 'cash-tape', alt: 'Кассовая (чековая) лента, термолента для банкоматов и платежных терминалов' },
+            { target: 'check-tape', alt: 'Чековая лента однослойная офсетная' },
+            { target: 'fax-paper',  alt: 'Факс бумага' },
+            { target: 'termo-label',  alt: 'Термоэтикетка' },
+            { target: 'office-paper',  alt: 'Бумага офисная А4 80г.' },
+            { target: 'paper-lpu',  alt: 'Бумага перфорированая ЛПУ' },
+            { target: 'roll-paper', alt: 'Бумага рулонная' },
+            { target: 'plotter-rolls', alt: 'Ролики для плоттера' }
+        ];
+    };
+
     function getProductController (dataFile, columns) {
         return function ($log, $http) {
             var _this = this,
                 url = './data/' + dataFile + '.json';
 
+            this.id = dataFile;
             this.sortField = '';
             this.sortDirection = true;
 
@@ -40,9 +59,10 @@
         };
     }
 
-    var application = angular.module('app', []);
+    var application = angular.module('app', ['scrollto']);
     application.controller('DeliveryController', DeliveryController);
     application.controller('PaymentController', PaymentController);
+    application.controller('MenuController', MenuController);
     application.controller('CashTapeController', getProductController('cash-tape'));
     application.controller('CheckTapeController', getProductController('check-tape'));
     application.controller('FaxPaperController', getProductController('fax-paper'));
